851c4fab80037c0a8a0ef211903a583d8b11a7bc
Rasmus Husted Tanggaard <rasmus@tthost.dk>
2015-01-29 13:41:17 +0100

    Added Reference file and asset files

--- src/main/java/com/nikras/minecraftmod/MinecraftMod.java
+++ src/main/java/com/nikras/minecraftmod/MinecraftMod.java
@@ -1,5 +1,6 @@
 package com.nikras.minecraftmod;
 
+import com.nikras.minecraftmod.Reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.init.Blocks;
@@ -7,16 +8,15 @@ import net.minecraft.init.Blocks;
 /**
  * Created by Coding on 22-01-2015.
  */
-@Mod(modid = MinecraftMod.MODID, version = MinecraftMod.VERSION)
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+
 public class MinecraftMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
+        System.out.println("Nikolaj Sutter");
     }
 }
